45844176c6dea37cf2207402c6f04881b50f1fe3
Chyna <chynapblair@gmail.com>
2023-07-15 10:15:51 -0400

    Fetch API

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
-* {
-    margin: 0;
-    padding: 0;
-    font-family: sans-serif;
-    box-sizing: border-box;
-  }
+const button = document.getElementById('button');
+const quoteDiv = document.getElementById('quote');
+
+// Fetch quote from API
+const getQuote = () => {
+  fetch('https://api.quotable.io/random/')
+    .then((res) => {
+      if (!res.ok) {
+        throw new Error('Request failed. Please try again.');
+      }
+
+      return res.json();
+    })
+    .then((data) => displayQuote(data))
+    .catch((error) => (quoteDiv.innerHTML = error));
+};
+
+// Display quote to DOM
+const displayQuote = (quote) => {
+  quoteDiv.innerHTML = `"${quote.content}" <br> - ${quote.author}`;
+};
+
+// Event listeners
+button.addEventListener('click', getQuote);
+
+getQuote();
